137e028a3f018a0a4b629a97ec71e75735fb99e2
radex <radexpl@gmail.com>
2023-07-14 19:13:01 +0200

    clean up - remove playwright, i18n

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,15 +2,6 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import { ThemeProvider } from '@/components/ThemeProvider'
 
-import { dir } from 'i18next'
-import { SupportedLanguages, languages } from './i18n/settings'
-import { LanguageProvider } from './i18n/LanguageProvider'
-
-
-export async function generateStaticParams() {
-  return languages.map((lng) => ({ lng }))
-}
-
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -20,22 +11,14 @@ export const metadata = {
 
 export default function RootLayout({
   children,
-  params: {
-    lng
-  }
 }: {
   children: React.ReactNode,
-  params: {
-    lng: SupportedLanguages
-  }
 }) {
   return (
-    <html lang={lng} dir={dir(lng)} suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <LanguageProvider lng={lng}>
             {children}
-          </LanguageProvider>
         </ThemeProvider>
       </body>
     </html>
