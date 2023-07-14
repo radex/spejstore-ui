c7925b2316dddfb25490c7ce86d910336db6f6e3
radex <radexpl@gmail.com>
2023-07-14 21:00:57 +0200

    login: play around with tailwind layout

--- src/app/login/layout.tsx
+++ src/app/login/layout.tsx
@@ -13,7 +13,7 @@ interface SettingsLayoutProps {
 export default function SettingsLayout({ children }: SettingsLayoutProps) {
   return (
     <>
-      <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0 justify-center items-center h-screen">
+      <div className="flex flex-row space-y-8 lg:space-x-12 lg:space-y-0 justify-center p-3">
         <div className="flex-1 lg:max-w-2xl">{children}</div>
       </div>
     </>
