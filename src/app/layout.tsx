ea82d3d94dca3fb364ddd25817ff4f9f65f97675
radex <radexpl@gmail.com>
2023-07-15 11:02:39 +0200

    towards a more mobile app-like experience

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,12 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pl" suppressHydrationWarning>
+      <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=no"
+        />
+      </head>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           {children}
